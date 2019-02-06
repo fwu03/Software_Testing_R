6fb344b10a2c04bccf3d89e95789d679fcadc7ac
unknown <dglin1@gmail.com>
2019-02-05 16:48:08 -0800

    added flatterlist function stub

---
+++ src/flattenlist.R
@@ -0,0 +1,8 @@
+#' Flattens a list so that elements in embedded lists are 
+#' returned in a flat list
+#' 
+#' @param l A list
+#' @return A flat list that has all of the elements in l 
+flattenlist <- function(l) {
+  
+}
