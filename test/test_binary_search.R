fd43e826c8c074cccd2753c114c43c2ec8f9bb91
bailey lei <bailey.lei@gmail.com>
2019-02-08 20:19:34 -0800

    added test function for flatten_list_prime.R

--- test/test_binary_search.R
+++ test/test_binary_search.R
@@ -7,14 +7,16 @@
 test_that('Test the correctness of input data types',{
   correct_num_x <- 5
   incorrect_num_x <- "5"
-  
+
   correct_list <- list(1,2,3,4,5)
   incorrect_list <- list('a','f','e')
-  incorrect_list2 <- c(1,4,10)
+  correct_vector <- c(1,4,10)
+  incorrect_vector <- c(1, "hello",10)
   
   expect_error(binary_search(incorrect_num_x, correct_list)) #expect error for incorrect data type of x
   expect_error(binary_search(correct_num_x, incorrect_list)) #expect error for incorrect data type of list
-  expect_error(binary_search(correct_num_x, incorrect_list2)) #expect error for incorrect data type of list
+  expect_error(binary_search(incorrect_num_x, correct_vector)) #expect error for incorrect data type of x
+  expect_error(binary_search(correct_num_x, incorrect_vector)) #expect error for incorrect data type of vector
 })
 
 # test the correctness of the function
@@ -22,7 +24,7 @@ test_that('test the correctness of the function outputs', {
   x <- 5
   list1 <- list(1,2,3,4,5,6)
   list2 <- list(10,100,1000,1000)
-  
+
   expect_equal(binary_search(x, list1), list(TRUE,5,5)) #expect TRUE if x is in the list with its value and position
   expect_equal(binary_search(x, list2), list(FALSE,5,0)) #expect FALSE if x is not in the list and 0 for position
 })
@@ -32,11 +34,7 @@ test_that('test the range of function outputs',{
   x <- 5
   list1 <- list(1,2,3,4,5,6)
   list2 <- list(10,100,1000,1000)
-  
+
   expect_true(binary_search(x, list1)[3] %in% (0:length(list1))) #expect TRUE when x is in the list, and function's output still within the range
   expect_true(binary_search(x, list2)[3] %in% (0:length(list2))) #expect TRUE when x isn't in the list, and function's output still within the range
 })
-
-
-
-
