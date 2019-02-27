6296907f37b2badc7844c5d15fcbf7a85611f7bf
Fan Wu <f32wu@edu.uwaterloo.ca>
2019-02-26 19:40:12 -0800

    update the test function for binary search

--- tests/testthat/test_binary_search.R
+++ tests/testthat/test_binary_search.R
@@ -2,39 +2,45 @@
 # Author: Fan Wu
 # Usage: This script is used to test the function for binary_search.R
 
+library(testthat)
+library(mlist)
+
+context("binary search")
+
 
 # test when the inputs have incorrect data type
 test_that('Test the correctness of input data types',{
   correct_num_x <- 5
   incorrect_num_x <- "5"
 
-  correct_list <- list(1,2,3,4,5)
-  incorrect_list <- list('a','f','e')
-  correct_vector <- c(1,4,10)
-  incorrect_vector <- c(1, "hello",10)
+  correct_lst <- c(1,2,3,4,5)
+  incorrect_lst <- c('a','f','e')
   
   expect_error(binary_search(incorrect_num_x, correct_list)) #expect error for incorrect data type of x
-  expect_error(binary_search(correct_num_x, incorrect_list)) #expect error for incorrect data type of list
-  expect_error(binary_search(incorrect_num_x, correct_vector)) #expect error for incorrect data type of x
-  expect_error(binary_search(correct_num_x, incorrect_vector)) #expect error for incorrect data type of vector
+  expect_error(binary_search(correct_num_x, incorrect_list)) #expect error for incorrect data type of lst
 })
 
-# test the correctness of the function
-test_that('test the correctness of the function outputs', {
+# test when the inputs have incorrect data value
+test_that('Test the correctness of input data values',{
+  correct_num_x <- 5
+  incorrect_num_x <- 5000
+  
+  correct_lst <- c(1,2,3,4,5)
+  incorrect_lst <- c(1,2000,3,4,5)
+  
+  expect_error(binary_search(incorrect_num_x, correct_list)) #expect error for x greater than 1000
+  expect_error(binary_search(correct_num_x, incorrect_list)) #expect error for lst contains value greater than 1000
+})
+
+# test the correctness of the function output
+test_that('Test the correctness of the function outputs', {
   x <- 5
-  list1 <- list(1,2,3,4,5,6)
-  list2 <- list(10,100,1000,1000)
+  lst1 <- c(1,2,3,4,5,6)
+  lst2 <- c(1,2,3)
 
-  expect_equal(binary_search(x, list1), list(TRUE,5,5)) #expect TRUE if x is in the list with its value and position
-  expect_equal(binary_search(x, list2), list(FALSE,5,0)) #expect FALSE if x is not in the list and 0 for position
+  expect_equal(binary_search(x, lst1), c('TRUE','5','5')) #expect TRUE if x is in the lst with its value and position
+  expect_equal(binary_search(x, lst2), c('FALSE','5',NA)) #expect FALSE if x is not in the lst and NA for position
+  expect_equal(is.vector(binary_search(x, lst1)), TRUE) #expect the output should be vector
+  expect_equal(length(binary_search(x, lst1)), 3) #expect the output should have length 3
 })
 
-# test the position value in function's output is within the range (i.e. 0 to length(list))
-test_that('test the range of function outputs',{
-  x <- 5
-  list1 <- list(1,2,3,4,5,6)
-  list2 <- list(10,100,1000,1000)
-
-  expect_true(binary_search(x, list1)[3] %in% (0:length(list1))) #expect TRUE when x is in the list, and function's output still within the range
-  expect_true(binary_search(x, list2)[3] %in% (0:length(list2))) #expect TRUE when x isn't in the list, and function's output still within the range
-})
